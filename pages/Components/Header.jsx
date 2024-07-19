514f22328ff5018e2867a135f5370d571c0bda14
Burunga <monte_marcos@hotmail.com>
2024-07-18 21:06:10 -0300

    feat: Importação dos módulos useState, useEffect e useRouter
    feat: Atribuída lógica de variável de estado nos Links. O elemento 'Link' que estiver selecionado irá receber uma estilização própria
    
    style: componente Header fica com estilização própria quando um dos Links foram selecionados

--- pages/Components/Header.jsx
+++ pages/Components/Header.jsx
@@ -1,18 +1,52 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 import styles from "@/styles/Header.module.css";
 
-export default function Header(props) {
+export default function Header() {
+  // variável router recebe as funções do módulo useRouter()
+  const router = useRouter();
+  // Váriável de estado
+  const [selectedLink, setSelectedLink] = useState(null);
+
+  // useEffect -> para executar sempre que o 'router.pathname' (rota) mudar
+  useEffect(() => {
+    // Objeto que mapeia cada rota para um índice correspondente
+    const caminhoPorIndice = {
+      "/": 0,
+      "/projects": 1,
+      "/contacts": 2,
+    };
+    // Atualiza o estado 'selectedLink' com o índice que corresponde a rota 'atual'
+    setSelectedLink(caminhoPorIndice[router.pathname]);
+  }, [router.pathname]);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} limit`}>
-        <Link className={styles.link} href="/">
+        <Link
+          className={`${styles.link} ${
+            selectedLink === 0 ? styles.selected : ""
+          }`}
+          href="/"
+        >
           Home
         </Link>
-        <Link className={styles.link} href="/projects">
+        <Link
+          className={`${styles.link} ${
+            selectedLink === 1 ? styles.selected : ""
+          }`}
+          href="/projects"
+        >
           Projetos
         </Link>
-        <Link className={styles.link} href="/contacts">
+        <Link
+          className={`${styles.link} ${
+            selectedLink === 2 ? styles.selected : ""
+          }`}
+          href="/contacts"
+        >
           Contatos
         </Link>
       </nav>
