198f0faf8a7cd6381622c1323ec349e241d97af5
Burunga <monte_marcos@hotmail.com>
2024-07-19 18:20:27 -0300

    feat: imagem do Menu que aparece em resolução até 500px, é uma rota que envia o usuário para a página principal do portifólio.

--- pages/Components/Menu.jsx
+++ pages/Components/Menu.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 import styles from "@/styles/Menu.module.css";
 
 export default function Menu(props) {
   return (
     <div className={`${styles.containerMenu} limit`}>
-      <i className="bi bi-file-code"></i>
+      <Link className={styles.image} href="/">
+        <i className="bi bi-file-code"></i>
+      </Link>
 
       <button className={styles.menu} onClick={props.funcao}>
         <i className="bi bi-menu-button"></i>
