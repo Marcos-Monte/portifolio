097bd4e67d214d773ff95ca607e282ae867f59e4
Burunga <monte_marcos@hotmail.com>
2024-09-11 19:40:25 -0300

    feat: adicionando projeto de gerador de visitas domiciliares; fix: alterando imagem dos projetos

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,10 +1,11 @@
 import Card from "./Components/Card";
 import Footer from "./Components/Footer";
-import Thiago from "/public/img/andrade.png";
 import CadastroGestante from "/public/img/cadastro-gestante.png";
 import Clinica from "/public/img/clinica.png";
 import JornadaPython from "/public/img/jornada-python.png";
 import Restaurant from "/public/img/restaurant.png";
+import Vd from "/public/img/solicitacaovd.png";
+import Thiago from "/public/img/thiagoandrade.png";
 
 import styles from "@/styles/Projects.module.css";
 
@@ -20,22 +21,17 @@ export default function Projects() {
             title="Thiago Andrade"
             image={Thiago}
             github="https://github.com/Marcos-Monte/thiago-andrade"
-            vercel="https://thiago-andrade.vercel.app/manifesto"
+            vercel="https://thiago-andrade.vercel.app"
           />
+
           <Card
-            subtitle="Menu Interativo"
-            title="Restaurant"
-            image={Restaurant}
-            github="https://github.com/Marcos-Monte/next-restaurant"
-            vercel="https://next-restaurant-ecru.vercel.app/"
+            subtitle="Gerador de Visita Domiciliar"
+            title="Unidade de Saúde"
+            image={Vd}
+            github="https://github.com/Marcos-Monte/vd-valongo"
+            vercel="https://web-site-clinica-react.vercel.app/"
           />
-          <Card
-            subtitle="Landing Page"
-            title="Jornada Python"
-            image={JornadaPython}
-            github="https://github.com/Marcos-Monte/Teste-Pratico-Hashtag"
-            vercel="https://teste-pratico-hashtag.vercel.app/"
-          />
+
           <Card
             subtitle="Página de Cadastro"
             title="Controle de Gestantes SUS"
@@ -43,6 +39,23 @@ export default function Projects() {
             github="https://github.com/Marcos-Monte/cadastro-gestantes"
             vercel="https://cadastro-gestantes.vercel.app/"
           />
+
+          <Card
+            subtitle="Landing Page"
+            title="Jornada Python"
+            image={JornadaPython}
+            github="https://github.com/Marcos-Monte/Teste-Pratico-Hashtag"
+            vercel="https://teste-pratico-hashtag.vercel.app/"
+          />
+
+          <Card
+            subtitle="Menu Interativo"
+            title="Restaurant"
+            image={Restaurant}
+            github="https://github.com/Marcos-Monte/next-restaurant"
+            vercel="https://next-restaurant-ecru.vercel.app/"
+          />
+
           <Card
             subtitle="Landing Page"
             title="Clínica Médica CM Life"
